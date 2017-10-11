fbd4c6681d70452335f8a5137f4f7da549161c04
TjDMC <tjustin.asdf@outlook.com>
2017-10-11 11:44:17 +0800

    Update pcfTable.sql

--- SQL Codes/pcfTable.sql
+++ SQL Codes/pcfTable.sql
@@ -1,7 +1,7 @@
-create table pcf{
+create table pcf(
 	pcf_id SMALLINT NOT NULL UNIQUE,
 	pcf_type VARCHAR(100) NOT NULL DEFAULT '',
-	pcf_partifculars varchar(100) default '',
+	pcf_partifculars VARCHAR(100) DEFAULT '',
 	pcf_supporting_documents VARCHAR(100) DEFAULT '',
 	pcf_screening_training FLOAT NOT NULL DEFAULT 0.0,
 	pcf_meals_snacks FLOAT NOT NULL DEFAULT 0.0,
@@ -11,5 +11,5 @@ create table pcf{
 	pcf_medical_supplies FLOAT NOT NULL DEFAULT 0.0,
 	pcf_rr_other_expenses FLOAT NOT NULL DEFAULT 0.0,
 	pcf_rr_op_desc VARCHAR(100) DEFAULT '',
-	primary key pcf_id
-}
+	primary key (pcf_id)
+);
