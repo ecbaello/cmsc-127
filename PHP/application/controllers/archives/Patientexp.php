d64bdf3bace56f1a3d58529d75d31f59a0d1d52d
ahotsuki <31463941+ahotsuki@users.noreply.github.com>
2017-11-14 06:42:44 +0800

    updated

--- PHP/application/controllers/archives/Patientexp.php
+++ PHP/application/controllers/archives/Patientexp.php
@@ -1,16 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Patientexp extends MY_DBcontroller {
+class Patientexp extends MY_Archcontroller {
 
 	public function __construct()
 	{
 		parent::__construct();
 		
 		$this->load->model('archive_patient_expenses_model');
-
 		$this->model = $this->archive_patient_expenses_model;
-
 		$this->model->init();
 	}
 	
