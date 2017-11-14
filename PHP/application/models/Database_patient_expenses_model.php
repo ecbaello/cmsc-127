fc43b90e5a8f25338277e69a8c8bcb3dbb7c5b2d
TjDMC <tjustin.asdf@outlook.com>
2017-11-14 10:29:33 +0800

    patient id

--- PHP/application/models/Database_patient_expenses_model.php
+++ PHP/application/models/Database_patient_expenses_model.php
@@ -19,6 +19,7 @@ class Database_patient_expenses_model extends MY_DBmodel
 	            )
        		);
 			$this->dbforge->add_field		($fields);
+			$this->dbforge->add_field		("pe_patient_id INT NOT NULL");
 			$this->dbforge->add_field		("pe_hospital_bill numeric(12,2) DEFAULT 0.0");
 			$this->dbforge->add_field		("pe_laboratory numeric(12,2) DEFAULT 0.0");
 			$this->dbforge->add_field		("pe_medical_supplies numeric(12,2) DEFAULT 0.0");
@@ -32,6 +33,7 @@ class Database_patient_expenses_model extends MY_DBmodel
 			$this->dbforge->create_table	($this->TableName);
 
 			$this->registerFieldTitle($this->TablePrimaryKey, '#');
+			$this->registerFieldTitle('pe_patient_id', 'Patient ID', 'NUMBER');
 			$this->registerFieldTitle('pe_hospital_bill', 'Hospital Bill', 'FLOAT');
 			$this->registerFieldTitle('pe_laboratory', 'Laboratory', 'FLOAT');
 			$this->registerFieldTitle('pe_medical_supplies', 'Medical Supplies', 'FLOAT');
