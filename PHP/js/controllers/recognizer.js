0e36023798377341512d9aa2279eac08b206ee70
rookiegrammer <30749394+rookiegrammer@users.noreply.github.com>
2017-12-01 13:23:15 +0800

    Revert "Revert "Revert "Revert "delete""""
    This reverts commit b90dca069c3e5defdb29d2acd94b24b741211290.

---
+++ PHP/js/controllers/recognizer.js
@@ -0,0 +1,51 @@
+app.controller('selector', ['$scope', '$http', 'recognizerUrl', '$mdDialog', function($scope, $http, recognizerUrl, $mdDialog){
+
+	$scope.data = {};
+	$scope.recognizing = null;
+	$scope.title = '';
+
+	function loadOptions() {
+		requestget(
+			recognizerUrl+'/data',
+			null,
+			function (data) {
+				$scope.data = data.data;
+				$scope.$apply();
+
+				console.log($scope.data);
+			},
+			function () {
+
+			}
+		);
+	}
+
+	$scope.recognize = function(data) {
+		$scope.recognizing = data;
+		$mdDialog.show({
+			contentElement: '#recognizerDialog',
+			parent: angular.element(document.body),
+			targetEvent: $scope.$event,
+			clickOutsideToClose: true,
+			fullscreen: false
+		});
+	};
+
+	$scope.recognizeTable = function(data, name) {
+		requestpost(
+			recognizerUrl+'/identify',
+			{ table:data, title:name },
+			null,
+			function () {
+				loadOptions();
+			},
+			function () {}
+		);
+	};
+
+	$scope.closeDialog = function() {
+		$mdDialog.cancel();
+	};
+
+	loadOptions();
+}]);
